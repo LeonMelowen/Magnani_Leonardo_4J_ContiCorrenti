8e081a3b19329b301cf510e93d079b36c2c7ee54
LeonMelowen <63230158+LeonMelowen@users.noreply.github.com>
2020-05-05 08:09:21 +0200

    Initial commit

---
+++ Mandrelli_Magnani_Nadifi_ContiCorrenti-master/ContoCorrente.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandrelli_Magnani_Nadifi_ContiCorrenti_master
+{
+    class ContoCorrente//Dichiarazione classe Conto Corrente
+    {
+        protected string iban;
+        protected double saldo;
+        string username = null;
+        string password=null;
+        protected Intestatario intestatario;
+        protected List<Movimento> movimenti = new List<Movimento>(); //Lista di movimenti
+        int maxMovimenti;
+
+        public ContoCorrente(string iban, double saldo, Intestatario intestatario)//Costruttore classe
+        {
+            this.iban = iban;
+            this.saldo = saldo;
+            this.intestatario = intestatario;
+
+            maxMovimenti = 50;
+        }
+
+        public virtual string getUsername()
+        {
+            return username;
+        }
+
+        public virtual string getPassword()
+        {
+            return password;
+        }
+
+        public virtual string getNomeintestatario()//Metodo che restituisce nome intestatario
+        {
+            return intestatario.getNome();
+        }
+
+        public virtual string getCodiceFiscaleintestatario()
+        {
+            return intestatario.getCodiceFiscale();
+        }
+
+        public virtual string getIban()//Metodo che restituisce Iban
+        {
+            return iban;
+        }
+
+        public virtual double getSaldo()//Metodo che restituisce saldo
+        {
+            return saldo;
+        }
+
+        public virtual void setSaldo(double importo) //Aggiorna il saldo del conto
+        {
+            saldo = saldo + importo;
+        }
+
+        public virtual void AddMovimenti(Movimento movimento)//Viene aggiunto un movimento alla lista dei movimenti
+        {
+            movimenti.Add(movimento);
+        }
+
+        public virtual List<Movimento> getMovimenti()
+        {
+            return movimenti;
+        }
+
+        public virtual int getNumeroMovimenti()
+        {
+            return movimenti.Count();
+        }
+
+        public virtual int getMaxMovimenti()
+        {
+            return maxMovimenti;
+        }
+
+    }
+
+    class ContoOnline : ContoCorrente
+    {
+        double maxPrelievo;
+        string username;
+        string password;
+        int maxMovimenti;
+
+        public ContoOnline(string username, string password, string iban, double saldo, Intestatario intestatario) : base(iban, saldo, intestatario)
+        {
+            maxPrelievo = 3000;
+            this.username = username;
+            this.password = password;
+            maxMovimenti = 50;
+        }
+
+        public override void setSaldo(double importo)
+        {
+            if (importo > 0 || (importo < 0 && importo >= -maxPrelievo))
+            {
+                saldo = saldo + importo;
+            }
+        }
+
+        public override string getUsername()
+        {
+            return username;
+        }
+
+        public override string getPassword()
+        {
+            return password;
+        }
+
+        public override double getSaldo()
+        {
+            return saldo;
+        }
+
+        public override string getIban()
+        {
+            return iban;
+        }
+
+        public override string getNomeintestatario()
+        {
+            return intestatario.getNome();
+        }
+
+        public override string getCodiceFiscaleintestatario()
+        {
+            return intestatario.getCodiceFiscale();
+        }
+
+        public override void AddMovimenti(Movimento movimento)
+        {
+            movimenti.Add(movimento);
+        }
+
+        public override List<Movimento> getMovimenti()
+        {
+            return movimenti;
+        }
+
+        public override int getNumeroMovimenti()
+        {
+            return movimenti.Count();
+        }
+
+        public override int getMaxMovimenti()
+        {
+            return maxMovimenti;
+        }
+
+    }
+
+    class Intestatario
+    {
+        string nome;
+        string cognome;
+        DateTime dataNascita;
+        string codiceFiscale;
+        string indirizzo;
+        string telefono;
+
+        public Intestatario(string nome, string cognome, DateTime dataNascita, string codiceFiscale, string indirizzo, string telefono)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.dataNascita = dataNascita;
+            this.codiceFiscale = codiceFiscale;
+            this.indirizzo = indirizzo;
+            this.telefono = telefono;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+        public DateTime getDataNascita()
+        {
+            return dataNascita;
+        }
+
+        public string getCodiceFiscale()
+        {
+            return codiceFiscale;
+        }
+
+        public string getIndirizzo()
+        {
+            return indirizzo;
+        }
+
+        public string getTelefono()
+        {
+            return telefono;
+        }
+    }
+    
+
+    class Banca
+    {
+        string nome;
+        string indirizzo;
+        List<Intestatario> intestatari;
+        List<ContoCorrente> conti; 
+
+        public Banca(string nome, string indirizzo)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            conti = new List<ContoCorrente>(); //Lista di conti
+            intestatari= new List<Intestatario>();
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getIndirizzo()
+        {
+            return indirizzo;
+        }
+
+        public void AddIntestatario(Intestatario i)
+        {
+            intestatari.Add(i);
+        }
+
+        public void AddConto(ContoCorrente c)//Viene aggiunto un conto alla lista dei conti
+        {
+            conti.Add(c);
+        }
+
+        public List<ContoCorrente> getConti()
+        {
+            return conti;
+        }
+
+        public List<Intestatario> getIntestatari()
+        {
+            return intestatari;
+        }
+
+    }
+
+    class Movimento
+    {
+        protected double importo;
+        protected DateTime dataOra;
+
+        public Movimento(double importo, DateTime dataOra)
+        {
+            this.importo = importo;
+            this.dataOra = dataOra;           
+        }
+
+        public virtual void Sommare(ContoCorrente conto)
+        {
+            conto.setSaldo(importo);//Importo movimento nel conto corrente
+        }
+
+        public virtual double getImporto()//Restituisce l'importo del movimento
+        {
+            return importo;
+        }
+
+        public virtual DateTime getDataOra()//Restituisce l'orario del movimento
+        {
+            return dataOra;
+        }
+    }
+
+    class Prelievo : Movimento
+    {
+        public Prelievo(double importo, DateTime dataOra) : base(importo, dataOra)
+        {
+
+        }
+
+        public override void Sommare(ContoCorrente conto)//Effettua un prelievo dal conto corrente
+        {
+            importo = -importo;
+            conto.setSaldo(importo);
+        }
+
+        public override double getImporto()//Restituisce l'importo del prelieve
+        {
+            return importo;
+        }
+
+        public override DateTime getDataOra()//Restituisce l'orario del versamento
+        {
+            return dataOra;
+        }
+    }
+
+    class Versamento : Movimento
+    {
+        public Versamento(double importo, DateTime dataOra) : base(importo, dataOra)
+        {
+
+        }
+
+        public override void Sommare(ContoCorrente conto)//Importo versamento nel conto corrente
+        {
+            conto.setSaldo(importo);
+        }
+
+        public override double getImporto()//Restituisce l'importo del versamento
+        {
+            return importo;
+        }
+
+        public override DateTime getDataOra()//Restituisce l'orario del versamento
+        {
+            return dataOra;
+        }
+    }
+
+    class Bonifico : Movimento
+    {
+        double commissione;
+
+        public Bonifico(double importo, DateTime dataOra) : base(importo, dataOra)
+        {
+            commissione=importo*0.015;
+        }
+
+        public void EseguiBonifico(ContoCorrente versa, ContoCorrente preleva)//Metodo che ci permette di eseguire un bonifico, andando a prelevare i soldi da un conto per versarli in quello del destinatario
+        {
+            versa.setSaldo(importo-commissione);
+            importo = -importo;
+            preleva.setSaldo(importo);
+
+        }
+
+        public string getMittente(ContoCorrente versa)//Restituisce il mittente del bonifico
+        {
+            return versa.getNomeintestatario();
+        }
+
+        public double getCommissione()
+        {
+            return commissione;
+        }
+    }
+
+}
---
+++ Mandrelli_Magnani_Nadifi_ContiCorrenti-master/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandrelli_Magnani_Nadifi_ContiCorrenti_master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creazione di una banca
+            string nomeBanca;
+            string indirizzoBanca;
+            string nome;
+            string cognome;
+            DateTime dataNascita;
+            DateTime dataMovimento;
+            string codiceFiscale;
+            string indirizzo;
+            string telefono;
+            string iban;
+            string iban2;
+            double saldo;
+            string username;
+            string password;
+            double quantita;
+            string scelta="";
+            string scelta2="";
+            int maxMovimenti;
+            int nMovimenti;
+
+            Console.Write("Nome Banca: ");
+            nomeBanca=Console.ReadLine();
+            Console.Write("Indirizzo Banca: ");
+            indirizzoBanca = Console.ReadLine();
+            Banca DB = new Banca(nomeBanca, indirizzoBanca);
+
+            do {
+
+                Console.Write("Nome Intestatario: ");
+                nome = Console.ReadLine();
+                Console.Write("Cognome Intestatario: ");
+                cognome = Console.ReadLine();
+                Console.Write("Data di nascita Intestatario: ");
+                dataNascita = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Codice fiscale Intestatario: ");
+                codiceFiscale = Console.ReadLine();
+                Console.Write("Indirizzo Intestatario: ");
+                indirizzo = Console.ReadLine();
+                Console.Write("Numero telefono Intestatario: ");
+                telefono = Console.ReadLine();
+
+                // Creazione del primo intestatario e del suo relativo conto corrente (tradizionale)
+                Intestatario cliente = new Intestatario(nome, cognome, dataNascita, codiceFiscale, indirizzo, telefono);
+                DB.AddIntestatario(cliente);
+                
+                //Creazione conti
+                do {
+
+                    do {
+                        Console.Write("Vuole creare un nuove conto? (Y/N)");
+                        scelta = Console.ReadLine();
+                    } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+                    if (scelta == "y" || scelta == "Y")
+                    {
+                        do
+                        {
+                            Console.Write("Che cosa vuole fare? \n 1.ContoCorrente \n 2.ContoOnline \n 0.BACK \n\n Scelta:");
+                            scelta2 = Console.ReadLine();
+                        } while (scelta2 != "1" && scelta2 != "2" && scelta2 != "0");
+
+                        switch (scelta2)
+                        {
+                            case "1":
+                                Console.Write("Iban del conto:");
+                                iban = Console.ReadLine();
+                                Console.Write("Saldo del conto:");
+                                saldo = Convert.ToDouble(Console.ReadLine());
+                                DB.AddConto(new ContoCorrente(iban, saldo, cliente));
+                               break;
+                            case "2":
+                                Console.Write("Username:");
+                                username = Console.ReadLine();
+                                Console.Write("Password:");
+                                password = Console.ReadLine();
+                                Console.Write("Iban del conto:");
+                                iban = Console.ReadLine();
+                                Console.Write("Saldo del conto:");
+                                saldo = Convert.ToDouble(Console.ReadLine());
+                                DB.AddConto(new ContoOnline(username, password, iban, saldo, cliente));
+                                break;
+                        }
+                 
+                        do
+                        {
+                            Console.Write("Vuole aggiungere un altro conto?: (Y/N)");
+                            scelta = Console.ReadLine();
+                        } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+                    }
+                
+                } while (scelta2=="0" || (scelta=="Y" || scelta=="y"));
+
+
+                //Creazione movimenti
+                do
+                {
+
+                    do
+                    {
+                        Console.Write("Vuole fare dei movimenti?: (Y/N)");
+                        scelta = Console.ReadLine();
+                    } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+                    if (scelta == "y" || scelta == "Y")
+                    {
+                        do
+                        {
+                            Console.Write("Che cosa vuole fare? \n 1.Prelievo \n 2.Versamento \n 3.Bonifico \n 0.BACK \n");
+                            scelta2 = Console.ReadLine();
+                        } while (scelta2 != "1" && scelta2 != "2" && scelta2 != "3" && scelta2!="0");
+
+                        switch (scelta2)
+                        {
+                            case "1":
+                                Console.Write("Iban del conto in cui vuole prelevare: ");
+                                iban=Console.ReadLine();
+                                Console.Write("Quanto vuole prelevare?: ");
+                                quantita = Convert.ToDouble(Console.ReadLine());
+                                Console.Write("Data prelievo?: ");
+                                dataMovimento = Convert.ToDateTime(Console.ReadLine());
+
+                                bool controllo = false;
+                                foreach (ContoCorrente conto in DB.getConti())
+                                {
+                                    if (iban == conto.getIban())
+                                    {
+                                        maxMovimenti = conto.getMaxMovimenti();
+                                        nMovimenti = conto.getNumeroMovimenti();
+
+                                        if (nMovimenti < maxMovimenti)
+                                        {
+                                            if (conto is ContoCorrente)
+                                            {
+                                                if (conto.getSaldo() >= quantita)
+                                                {
+                                                    Prelievo p = new Prelievo(quantita, dataMovimento);
+                                                    conto.AddMovimenti(p);
+                                                    p.Sommare(conto);
+                                                    controllo = true;
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("Non puo prelevare di piu del saldo nel conto = " + conto.getSaldo());
+                                                    controllo = true;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                if (conto.getSaldo() >= quantita)
+                                                {
+                                                    if (quantita <= 3000)
+                                                    {
+                                                        Prelievo p = new Prelievo(quantita, dataMovimento);
+                                                        conto.AddMovimenti(p);
+                                                        p.Sommare(conto);
+                                                        controllo = true;
+                                                    }
+                                                    else
+                                                    {
+                                                        Console.WriteLine("Puo prelevare al massimo 3000 ad ogni prelevazione");
+                                                        controllo = true;
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("Non puo prelevare di piu del saldo nel conto = " + conto.getSaldo());
+                                                    controllo = true;
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Superato il limite dei movimenti = " + conto.getMaxMovimenti());
+                                            controllo = true;
+                                        }                                    
+                                    }
+                                }
+                                if (controllo == false)
+                                {
+                                    Console.WriteLine("Non è stato trovato un conto con questo iban..");
+                                }
+
+                                do
+                                {
+                                    Console.Write("Vuole fare un altro movimento?: (Y/N)");
+                                    scelta = Console.ReadLine();
+                                } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+                                break;
+
+                            case "2":
+                                Console.Write("Iban del conto in cui vuole versare: ");
+                                iban = Console.ReadLine();
+                                Console.Write("Quanto vuole versare?: ");
+                                quantita = Convert.ToDouble(Console.ReadLine());
+                                Console.Write("Data varsamento?: ");
+                                dataMovimento = Convert.ToDateTime(Console.ReadLine());
+
+                                bool controllo2 = false;
+                                foreach (ContoCorrente conto in DB.getConti())
+                                {
+                                    if (iban == conto.getIban())
+                                    {
+                                         maxMovimenti = conto.getMaxMovimenti();
+                                         nMovimenti = conto.getNumeroMovimenti();
+                                        if (nMovimenti < maxMovimenti)
+                                        {
+                                            Versamento v = new Versamento(quantita, dataMovimento);
+                                            conto.AddMovimenti(v);
+                                            v.Sommare(conto);
+                                            controllo2 = true;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Superato il limite dei movimenti = " + conto.getMaxMovimenti());
+                                            controllo = true;
+                                        }                                                                 
+                                    }
+                                }
+                                if (controllo2 == false)
+                                {
+                                    Console.WriteLine("Non è stato trovato un conto con questo iban..");
+                                }
+
+                                do
+                                {
+                                    Console.Write("Vuole fare un altro movimento?: (Y/N)");
+                                    scelta = Console.ReadLine();
+                                } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+                                break;
+
+                            case "3":
+                                Console.Write("Iban del conto in cui vuole prelevare: ");
+                                iban2 = Console.ReadLine();
+                                Console.Write("Iban del conto in cui vuole versare: ");
+                                iban = Console.ReadLine();
+                                Console.Write("Quanto vuole versare/prelevare?: ");
+                                quantita = Convert.ToDouble(Console.ReadLine());
+                                Console.Write("Data bonifico?: ");
+                                dataMovimento = Convert.ToDateTime(Console.ReadLine());
+
+                                bool controllo3 = false;
+                                ContoCorrente posto = null;
+
+                                Bonifico b = new Bonifico(quantita, dataMovimento);
+
+                                foreach (ContoCorrente conto in DB.getConti())
+                                {
+                                    if (iban == conto.getIban())
+                                    {
+                                        posto = conto;                                                                                                            
+                                    }
+                                }
+
+                                if (posto != null)
+                                {
+                                    foreach (ContoCorrente conto in DB.getConti())
+                                    {
+                                        if (iban2 == conto.getIban())
+                                        {
+                                            conto.AddMovimenti(b);
+                                            b.EseguiBonifico(posto, conto);
+                                            controllo3 = true;
+                                        }
+                                    }
+                                }
+
+                                if (controllo3 == false)
+                                {
+                                    Console.WriteLine("Non è stato trovato uno dei 2 conti con questi iban..");
+                                }
+
+                                do
+                                {
+                                    Console.Write("Vuole fare un altro movimento?: (Y/N)");
+                                    scelta = Console.ReadLine();
+                                } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+                                break;
+                        }
+                    }
+
+                } while (scelta2 == "0" || (scelta == "Y" || scelta == "y"));
+
+                do
+                {
+                    Console.Write("Vuole creare un nuovo intestatario?: (Y/N)");
+                    scelta = Console.ReadLine();
+                } while (scelta != "y" && scelta != "Y" && scelta != "n" && scelta != "N");
+
+            } while (scelta=="Y" || scelta=="y");
+
+            Console.WriteLine("\n");
+            // Stampa info intestatario e del relativo conto 
+            foreach (Intestatario intestatario in DB.getIntestatari())
+            {
+                Console.WriteLine("\nIntestatario: " + intestatario.getNome() + " " + intestatario.getCognome() + "\nData di nascita: " + intestatario.getDataNascita() + "\nAbita in via: " + intestatario.getIndirizzo() + "\nNumero di telefono: " + intestatario.getTelefono());
+                foreach (ContoCorrente conto in DB.getConti())
+                {
+                    if (conto.getCodiceFiscaleintestatario() == intestatario.getCodiceFiscale())
+                    {                       
+                            Console.WriteLine("\nCONTO: ");
+                            Console.WriteLine("Stampa del saldo di " + intestatario.getNome() + " " + intestatario.getCognome() + ": " + conto.getSaldo());
+                            // Vengono effettuati un versamento e un prelievo con la relativa stampa aggiornata
+                            Console.WriteLine("MOVIMENTI:");
+                            foreach (Movimento movimento in conto.getMovimenti())
+                            {
+                                movimento.Sommare(conto);
+                                Console.Write(movimento.getImporto() + " euro" + " fatto nel: " + movimento.getDataOra() + " - saldo attuale: " + conto.getSaldo() + "\n");
+                            }
+                            Console.WriteLine("\n");                                            
+                    }
+                }
+                Console.WriteLine("__________________________________________________________________________________________________");
+            }
+            Console.ReadLine();
+        }
+    }
+}
